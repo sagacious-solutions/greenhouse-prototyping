e392eb4b5381bf2db655c2b6855e8fbf9ac153ce
Matt Macdonald <mksmacdonald@gmail.com>
2023-12-17 17:23:08 -0800

    Lcd working

--- modules/I2C_LCD.py
+++ modules/I2C_LCD.py
@@ -6,7 +6,8 @@ import board
 
 
 class I2C_LCD :
-    def __init__ (self, num_cols=20, num_rows=4, pinMap1=True):
+    def __init__ (self, num_cols=20, num_rows=4, pinMap1=True, backlight_on=True):
+        self.backlight_on = backlight_on
         self.cols = num_cols
         self.row = num_rows
         self.pin_map = PinMapping.MAPPING1 if pinMap1 else PinMapping.MAPPING2
@@ -23,3 +24,20 @@ class I2C_LCD :
     def print(self, str, row=0, col=0):
         self._lcd.set_cursor_pos(row, col)
         self._lcd.print(str)
+
+    def clear(self):
+        self._lcd.clear()
+
+    def set_backlight(self):
+        self._lcd.set_backlight(self.backlight_on)
+
+    def toggle_backlight(self):
+        self.backlight_on = not self.backlight_on
+        self.set_backlight()
+
+lcd = I2C_LCD()
+import time
+time.sleep(2)
+lcd.toggle_backlight()
+time.sleep(2)
+lcd.toggle_backlight()
